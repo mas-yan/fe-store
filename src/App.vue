d1399ee68f32fc5c16f89be20938c22fa7524275
mas-yan <mrey5597@gmail.com>
2021-10-28 21:02:04 +0700

    custom bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="container my-5">
+  <div class="ro">
+    <div class="col">
+
+      <div class="bg-primary">
+        <div class="col-4">
+
+        <div class="card sekunder">
+          <div class="card-body">
+            <button class="btn-primary border-0 btn text-white shadow">haooooooooooooooooooooooooooooo</button>
+          </div>
+        </div>
+        </div>
+      </div>
+  
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
