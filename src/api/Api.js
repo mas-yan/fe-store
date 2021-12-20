530d5e15d107233e3ebd3cfc052553bc15572a74
mas-yan <mrey5597@gmail.com>
2021-12-20 20:25:39 +0700

    deploy

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const Api = axios.create({
-    baseURL: 'http://i-book-store.herokuapp.com/api'
+    baseURL: 'https://i-book-store.herokuapp.com/api'
 })
 
 export default Api
