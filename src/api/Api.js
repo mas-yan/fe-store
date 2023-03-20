f3c6e8951924ec99f3f0276d29af4988f2ed42c8
mas-yan <mrey5597@gmail.com>
2023-03-21 06:09:01 +0700

    update: api

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const Api = axios.create({
-    baseURL: 'https://semestaku.com/api'
+    baseURL: 'https://i-book-store.up.railway.app/api'
 })
 
 export default Api
